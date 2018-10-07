55fc95688f6141550c12556b00d83a47967d051d
rodrigosantosti <rodrigosantos17silva@gmail.com>
2018-10-07 19:12:37 -0300

    Exercicios 06 recursividade

---
+++ Exercicio6/atv09.py
@@ -0,0 +1,13 @@
+# 9 Defna a função temPrimoQ que recebe como argumento uma lista de listas de
+# números inteiros w e devolve True se alguma das sublistas w tem um número
+# primo e False em caso contrário.
+# Ex: temPrimoQ([[4,4,4,4],[5,4,6,7],[2,4,3]]) = True
+# Ex: temPrimoQ([[4,4,4,4],[4,4,4],[],[4]]) = False
+
+
+def temPrimoQ(lista):
+
+
+
+print(temPrimoQ([[4,4,4,4],[5,4,6,7],[2,4,3]]))
+print(temPrimoQ([[4,4,4,4],[4,4,4],[],[4]]))
